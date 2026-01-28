c9b208768386f9f1839d77b98739f51af5187025
Nita <242255492+n-guy-en@users.noreply.github.com>
2026-01-28 13:19:03 +0100

    Warmtebron update
    - Heerenveen, Leeuwarden en Harlingen light versie weergave
    - Andere woonplaatsen zijn full weergave

--- data/scripts/geojson.py
+++ data/scripts/geojson.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 
 # 1. Laad GeoJSON
 gdf = gpd.read_file(
-    "/Users/anitavn/Documents/Warmtekaart_test/streamlit/Untitled/data/layers/warmtenet_full.geojson"
+    ""
 )
 
 # 2. Zet CRS als het niet in het bestand staat
@@ -19,8 +19,8 @@ gdf_4326.to_file("warmtenet_full.geojson", driver="GeoJSON")
 import gzip
 import shutil
 
-input_file = "/Users/anitavn/Documents/Warmtekaart_test/streamlit/Untitled/data/layers/wegennet_friesland.geojson"
-output_file = "wegennet_frl.geojson.gz"
+input_file = "/Users/anguyen/Documents/GitHub/Warmtekaart/data/layers/warmtenet_full.geojson"
+output_file = "warmtenet_full.geojson.gz"
 
 with open(input_file, "rb") as f_in:
     with gzip.open(output_file, "wb") as f_out:
